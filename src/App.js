fc4cc5852e6c8c27a46d628ba812b22519dfd914
jr1412594 <jr.medina1412@gmail.com>
2020-11-20 13:26:52 -0700

    event

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import { Component } from 'react'
 
 import './App.css';
 
+const TvShowsURL = "http://api.tvmaze.com/shows"
+
 class App extends Component {
 
+  state = {
+    tvShows: []
+  }
+  
+  getTvShows = () => {
+    fetch(TvShowsURL)
+      .then(response => response.json())
+      .then(tvShows => this.setState({ tvShows}))
+  }
+  
   render(){
     return (
       <div className="App">
-  
+        <h1>Tv Shows !</h1>
+        <button onClick={this.getTvShows}>Get Shows</button>
       </div>
     );
   }
